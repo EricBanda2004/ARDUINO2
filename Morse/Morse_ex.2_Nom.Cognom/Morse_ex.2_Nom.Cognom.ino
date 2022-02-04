56a1830d37b6866563867d1c6e08b26cd035cab2
EricBanda2004 <eric.banda.35@lacetania.cat>
2022-02-04 20:12:35 +0100

    Arduino

---
+++ Morse/Morse_ex.2_Nom.Cognom/Morse_ex.2_Nom.Cognom.ino
@@ -0,0 +1,99 @@
+/******************************************************************************
+**                                                                           **
+**                              Morse                                        **
+**                                                                           **
+**                                                                           **
+******************************************************************************/
+
+//****** Includes *************************************************************
+
+//****** Variables ************************************************************
+const int pin = 8;   // pin for speaker
+
+int note = 1047;
+int velocitat = 10;
+
+//****** Setup ****************************************************************
+void setup()
+{
+}
+
+//****** Loop *****************************************************************
+void loop()
+{
+  //E   ** Exemple SOS **
+  punt();
+  //Espai entre lletres
+  espaiL();
+  //R
+  punt();
+  ratlla();
+  punt();
+  //Espai entre lletres
+  espaiL();
+  //I
+  punt();
+  //Espai entre lletres
+  espaiL();
+  //C
+  ratlla();
+  punt();
+  ratlla();
+  punt();
+  //Espai entre paraules
+  espaiP();
+  //B
+  ratlla();
+  punt();
+  punt();
+  punt();
+  //Espai entre lletres
+  espaiL();
+  //A
+  punt();
+  ratlla();
+  //Espai entre lletres
+  espaiL();
+  //N
+  ratlla();
+  punt();
+   //Espai entre lletres
+  espaiL();
+  //D
+  ratlla();
+  punt();
+  punt();
+  //Espai entre lletres
+  espaiL();
+  //A
+  punt();
+  ratlla();
+  //Espai entre paraules
+  espaiP();
+}
+
+//****** Funcions *************************************************************
+void punt()
+{
+  tone(pin, note, 1000/velocitat);
+  delay(1000/velocitat); 
+  delay(1000/velocitat); 
+}
+
+void ratlla()
+{
+  tone(pin, note, 3*(1000/velocitat));
+  delay(3*(1000/velocitat)); 
+  delay(1000/velocitat);  
+}
+
+void espaiL()  //Espai entre lletres
+{  
+  delay(2*(1000/velocitat));  
+}
+
+void espaiP()  //Espai entre paraules
+{   
+  delay(6*(1000/velocitat));  
+}
+
